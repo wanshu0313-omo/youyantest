96cd774caf80d5002c2dc9fdd6c533a771e81174
wanshu0313-omo <zhangjunchao0313@gmail.com>
2026-06-29 00:34:29 +0900

    Update lesson-order.js

--- data/lesson-order.js
+++ data/lesson-order.js
@@ -1,2 +1,2 @@
 /* 新增每日课程后，把课程 key 放在这里，日期轨道会自动显示。 */
-window.LESSON_ORDER = ["yesterday", "today", "lesson20260626", "lesson20260627", "lesson20260628"];
+window.LESSON_ORDER = ["yesterday", "today", "lesson20260626", "lesson20260627", "lesson20260628", "lesson20260629"];
